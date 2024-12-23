d3e70a3ea2c195ac8a29a2dba721ba48a43792d8
joonhoMin <joonho535@gmail.com>
2024-12-23 15:20:26 +0900

    work3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         App app=new App();
@@ -7,7 +9,22 @@ public class Main {
 
 class App{
     public void run(){
+//        == 명언 앱 ==
+//        명령) 등록
+//        명언 : 현재를 사랑하라.
+//        작가 : 작자미상
+//        명령) 종료
+
+        Scanner scanner=new Scanner(System.in);
+
         System.out.println("== 명언 앱 ==");
+
+        System.out.println("명령) 등록");
+        System.out.print("명언 : ");
+        scanner.nextLine();
+        System.out.print("작가 : ");
+        scanner.nextLine();
+
         System.out.println("명령) 종료");
     }
 }
