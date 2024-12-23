e6279d8601f608dd6d7db90fdca8cc112caa4c03
joonhoMin <joonho535@gmail.com>
2024-12-23 15:35:34 +0900

    work4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,18 +13,34 @@ class App{
 //        명령) 등록
 //        명언 : 현재를 사랑하라.
 //        작가 : 작자미상
+//        1번 명언이 등록되었습니다.
+//        명령) 등록
+//        명언 : 현재를 사랑하라.
+//        작가 : 작자미상
+//        2번 명언이 등록되었습니다.
 //        명령) 종료
 
         Scanner scanner=new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
 
-        System.out.println("명령) 등록");
-        System.out.print("명언 : ");
-        scanner.nextLine();
-        System.out.print("작가 : ");
-        scanner.nextLine();
+        while(true)
+        {
+            System.out.print("명령) ");
+            String command=scanner.nextLine();
 
-        System.out.println("명령) 종료");
+            if(command.equals("등록")){
+                System.out.print("명언 : ");
+                scanner.nextLine();
+                System.out.print("작가 : ");
+                scanner.nextLine();
+
+                System.out.println("1번 명령이 등록되었습니다.");
+            }
+
+            if(command.equals("종료")){
+                break;
+            }
+        }
     }
 }
