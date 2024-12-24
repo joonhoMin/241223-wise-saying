9178e10fb6bf201272a06ce699b01d2ca3f24684
joonhoMin <joonho535@gmail.com>
2024-12-24 12:30:20 +0900

    phase 8

---
+++ src/main/java/명언.java
@@ -0,0 +1,36 @@
+public class 명언 {
+    private int 번호;
+    private String 명언내용;
+    private String 작가;
+
+
+    public int get번호() {
+        return 번호;
+    }
+
+    public void set번호(int 번호) {
+        this.번호 = 번호;
+    }
+
+    public String get명언내용() {
+        return 명언내용;
+    }
+
+    public void set명언내용(String 명언내용) {
+        this.명언내용 = 명언내용;
+    }
+
+    public String get작가() {
+        return 작가;
+    }
+
+    public void set작가(String 작가) {
+        this.작가 = 작가;
+    }
+
+    public 명언(int 번호, String 명언내용, String 작가) {
+        this.번호 = 번호;
+        this.명언내용 = 명언내용;
+        this.작가 = 작가;
+    }
+}
